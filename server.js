e8e6c91a9726eb5b52a0f97e1e333c9de41b7148
chandan.balsamanta <chandan.balsamanta@nettantra.net>
2023-02-06 18:22:17 +0530

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const connectDB=require('./config/connect')
-// create express app
+
 var app = express();
 
 // parse application/x-www-form-urlencoded
@@ -9,39 +8,62 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
 app.use(bodyParser.json())
-const mongoose=require('mongoose');
 
 
-// Configuring the database
-// var dbConfig = require('./config/connect.js');
-// var mongoose = require('mongoose');
 
-// mongoose.Promise = global.Promise;
 
-// mongoose.connect(dbConfig.url, {
-// 	useMongoClient: true
-// });
-// mongoose.set('strictQuery', false);
-connectDB()
 
-// mongoose.connection.on('error', function() {
-//     console.log('Could not connect to the database. Exiting now...');
-//     process.exit();
-// });
-// mongoose.connection.once('open', function() {
-//     console.log("Successfully connected to the database");
-// })
+app.post('/message',function(req,res){
 
-// define a simple route
-app.get('/', function(req, res){
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    console.log("data",req.body)
+
+
+
+    var mqtt = require('mqtt')
+
+    var options = {
+        host: 'f48ee08ab11a445eb674d5c74cbc9aa8.s2.eu.hivemq.cloud',
+        port: 8883,
+        protocol: 'mqtts',
+        username: 'Chandan',
+        password: 'Chandan@1234'
+    }
+    
+    // initialize the MQTT client
+    var client = mqtt.connect(options);
+    
+    // setup the callbacks
+    client.on('connect', function () {
+        console.log('Connected');
+    });
+    
+    client.on('error', function (error) {
+        console.log(error);
+    });
+    
+    client.on('message', function (topic, message) {
+        // called each time a message is received
+        console.log('Received message:', topic, message.toString());
+    });
+    
+    // subscribe to topic 'my/test/topic'
+    client.subscribe('my/test/topic');
+    
+    // publish message 'Hello' to topic 'my/test/topic'
+    client.publish(req.body.topic, req.body.message);
+    res.send("done")
 });
 
-require('./app/routes/note.routes.js')(app);
-require('./app/routes/location.routes.js')(app);
+
+app.get('/', function(req, res){
+
+   
+    res.json({"message": "hello."});
+});
+
 
 // listen for requests
-let PORT=6000
+let PORT=3001
 app.listen(PORT, function(){
     console.log(`Server is listening on port ${PORT}`);
 });
